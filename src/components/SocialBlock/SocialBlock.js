e8e46a57a1ede615b63f18cd60b7d5ca9f60c31a
Eric Bond <ericbond007@gmail.com>
2018-03-14 16:53:33 -0400

    added github and facebook FA icons

--- src/components/SocialBlock/SocialBlock.js
+++ src/components/SocialBlock/SocialBlock.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import './SocialBlock.css';
-import FontAwesomeIcon from '@fortawesome/react-fontawesome'
 
 
 const SocialBlock = () => (
   <div className="socialBlock">
-    <p>
-      Github
-    </p>
-    <p>
-      Facebook
-    </p>
+    <i class="fab fa-github fa-5x" style={{ marginRight: '3rem', marginLeft: '3rem', marginBottom: '3rem'}}></i>
+    <i class="fab fa-facebook-square fa-5x" style={{ margin: '3rem'}}></i>
   </div>
 );
 
